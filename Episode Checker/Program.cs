cc78f3cd834859b0f253715f6b0a6ab74233752a
DaimaoOrochi <ryan.hert@yahoo.com>
2015-12-01 22:44:33 -0700

    first

---
+++ Episode Checker/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NLog;
+using NLog.Config;
+
+
+namespace Episode_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.google.com";
+           
+            var searchBox = driver.FindElement(By.Name("q"));
+            //Looks for the correct site.
+
+            searchBox.SendKeys("adblock plus");
+            
+
+            //searchBox.SendKeys("animefreak.tv");
+
+            searchBox.Submit();
+
+           var searchButton = driver.FindElement(By.ClassName("lsb"));
+           searchButton.Click();
+
+          
+
+
+
+            //Find the results of our query
+           driver.FindElement(By.LinkText("Adblock Plus - Surf the web without annoying ads!")).Click();           
+
+            
+
+            
+        }
+
+     
+    }
+}
