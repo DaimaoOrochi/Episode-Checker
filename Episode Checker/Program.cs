4ba12d46f901f2be237f26ff3ad4f4220fce3972
ryaher <ryan.hert@extendhealth.com>
2015-12-05 17:27:56 -0700

    this is my BOOMSTICK

--- Episode Checker/Program.cs
+++ Episode Checker/Program.cs
@@ -8,42 +8,49 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using NLog;
 using NLog.Config;
+using OpenQA.Selenium.Interactions;
 
 
 namespace Episode_Checker
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            IWebDriver driver = new FirefoxDriver();
+//            String AdBlockerFilePath = "C:'\'Users'\'ryaher'\'AppData'\'Roaming'\'Mozilla'\'Firefox'\'Profiles'\'mzzqocaz.default'\'adblockplus'\'elemhide.css";
+//            FirefoxProfile profile = new FirefoxProfile();       
+//            profile.AddExtension(AdBlockerFilePath);
+            //addExtension(new File(firebugFilePath));
+            
+            
+
+            FirefoxProfileManager profileManager = new FirefoxProfileManager();
+            FirefoxProfile profile = profileManager.GetProfile("Ryan");
+            IWebDriver driver = new FirefoxDriver(profile);
+            
             driver.Url = "http://www.google.com";
-           
+            
+
             var searchBox = driver.FindElement(By.Name("q"));
             //Looks for the correct site.
 
-            searchBox.SendKeys("adblock plus");
-            
+            searchBox.SendKeys("animefreak.tv");
 
-            //searchBox.SendKeys("animefreak.tv");
+
+        
 
             searchBox.Submit();
 
-           var searchButton = driver.FindElement(By.ClassName("lsb"));
-           searchButton.Click();
-
-          
-
-
-
-            //Find the results of our query
-           driver.FindElement(By.LinkText("Adblock Plus - Surf the web without annoying ads!")).Click();           
+            var searchButton = driver.FindElement(By.ClassName("lsb"));
+            searchButton.Click();
 
             
 
             
+
+            
+
+
         }
-
-     
     }
 }
