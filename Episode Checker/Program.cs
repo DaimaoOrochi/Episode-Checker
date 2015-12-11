9a9c8088230ae708221f2586a69105369e66489f
ryaher <ryan.hert@extendhealth.com>
2015-12-10 20:37:14 -0700

    This is my new commit

--- Episode Checker/Program.cs
+++ Episode Checker/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,22 +19,21 @@ namespace Episode_Checker
     {
         private static void Main(string[] args)
         {
-//            String AdBlockerFilePath = "C:'\'Users'\'ryaher'\'AppData'\'Roaming'\'Mozilla'\'Firefox'\'Profiles'\'mzzqocaz.default'\'adblockplus'\'elemhide.css";
-//            FirefoxProfile profile = new FirefoxProfile();       
-//            profile.AddExtension(AdBlockerFilePath);
-            //addExtension(new File(firebugFilePath));
             
-            
+            // creates a session that uses my Firefox profile.
 
             FirefoxProfileManager profileManager = new FirefoxProfileManager();
             FirefoxProfile profile = profileManager.GetProfile("Ryan");
             IWebDriver driver = new FirefoxDriver(profile);
             
+            
             driver.Url = "http://www.google.com";
             
 
-            var searchBox = driver.FindElement(By.Name("q"));
+            var searchBox = driver.FindElement(By.Id("lst-ib"));
             //Looks for the correct site.
+            bool dubPresent;
+            string episodeNumber;
 
             searchBox.SendKeys("animefreak.tv");
 
@@ -44,11 +45,41 @@ namespace Episode_Checker
             var searchButton = driver.FindElement(By.ClassName("lsb"));
             searchButton.Click();
 
-            
+            // Click on the first result for AnimeFreak.tv
+            driver.FindElement(By.XPath("id('rso')/div/div[1]/div/h3/a")).Click();
 
-            
+            System.Threading.Thread.Sleep(1000);
+            driver.FindElement(By.XPath("id('submenu')/ul/li[3]/a")).Click();
+            //Selects "O" from the list
+            driver.FindElement(By.XPath("id('primary')/div/div[3]/div[1]/div/div/span[20]/a")).Click();
+            //Goes to OnePiece
+            driver.FindElement(By.XPath(".//*[@id='primary']/div/div[3]/div[2]/table/tbody/tr[9]/td/a")).Click();
+            //Goes to Episode 422
+            driver.FindElement(By.XPath("id('page')/div[5]/div[2]/div[2]/div/ul/li[418]/a")).Click();
 
-            
+            switch (dubPresent)
+            {
+                case 1:
+
+                    try
+                    {
+                        driver.FindElement(By.XPath("id('primary')/x:div/x:div[3]/x:div/x:a[3]")).Click();
+                    
+                  
+                    
+
+
+
+
+            }
+
+
+
+
+
+
+
+
 
 
         }
