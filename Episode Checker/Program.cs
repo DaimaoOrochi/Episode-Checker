7ca92a72ca2ccc10676637efd6a7365f55b69852
ryaher <ryan.hert@extendhealth.com>
2015-12-10 21:31:32 -0700

    Loop logic working

--- Episode Checker/Program.cs
+++ Episode Checker/Program.cs
@@ -44,34 +44,37 @@ namespace Episode_Checker
 
             var searchButton = driver.FindElement(By.ClassName("lsb"));
             searchButton.Click();
-
+            System.Threading.Thread.Sleep(1000);
             // Click on the first result for AnimeFreak.tv
             driver.FindElement(By.XPath("id('rso')/div/div[1]/div/h3/a")).Click();
 
             System.Threading.Thread.Sleep(1000);
             driver.FindElement(By.XPath("id('submenu')/ul/li[3]/a")).Click();
+            System.Threading.Thread.Sleep(1000);
+
             //Selects "O" from the list
             driver.FindElement(By.XPath("id('primary')/div/div[3]/div[1]/div/div/span[20]/a")).Click();
+            System.Threading.Thread.Sleep(1000);
             //Goes to OnePiece
-            driver.FindElement(By.XPath(".//*[@id='primary']/div/div[3]/div[2]/table/tbody/tr[9]/td/a")).Click();
+
+             driver.FindElement(By.XPath(".//*[@id='primary']/div/div[3]/div[2]/table/tbody/tr[9]/td/a")).Click();
+            System.Threading.Thread.Sleep(1000);
+
             //Goes to Episode 422
-            driver.FindElement(By.XPath("id('page')/div[5]/div[2]/div[2]/div/ul/li[418]/a")).Click();
+            //driver.FindElement(By.XPath("id('page')/div[5]/div[2]/div[2]/div/ul/li[418]/a")).Click();
+            driver.FindElement(By.XPath("id('page')/div[5]/div[2]/div[2]/div/ul/li[1]/a")).Click();
+            System.Threading.Thread.Sleep(1000);
 
-            switch (dubPresent)
+
+            do
             {
-                case 1:
-
-                    try
-                    {
-                        driver.FindElement(By.XPath("id('primary')/x:div/x:div[3]/x:div/x:a[3]")).Click();
-                    
-                  
-                    
+                driver.FindElement(By.ClassName("page-next")).Click();
+            } while (driver.FindElement(By.XPath("id('primary')/div/div[3]/div/a[2]")).Enabled);
+            
+             driver.Close();
+            
 
 
-
-
-            }
 
 
 
